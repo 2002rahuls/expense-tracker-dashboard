6808cdf600cd069e9848b72fbdb10e5ec6e202f7
2002rahuls <rahulhs0212@gmail.com>
2026-02-21 03:57:38 +0530

    added cors

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -139,3 +139,4 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True
